5a9a0a5cf700fc1ddaa2969fc78cde2abbb32095
Neville Tummon <nevilletummon@Nevilles-MacBook-Pro.local>
2025-06-10 09:37:32 +1000

    optimise algo

--- dynamic_programming/longest_common_subsequence/algo.py
+++ dynamic_programming/longest_common_subsequence/algo.py
@@ -2,19 +2,18 @@ def longest_common_subsequence(text1: str, text2: str) -> int:
     cache = {}
 
     def dfs(n: int, m: int, length) -> int:
-        res = 0
+        res = length
+
+        if (n, m) in cache:
+            # print(f" cache[({n}, {m})] = {cache[(n, m)]}... already cached")
+            return cache[(n, m)]
 
         for i in range(n, len(text1)):
             for j in range(m, len(text2)):
-                if (i, j) in cache:
-                    # print(f" cache[({i}, {j})] = {cache[(i, j)]}... already cached")
-                    res = cache[(i, j)]
-                    continue
-
                 if text1[i] == text2[j]:
                     res = max(res, dfs(i+1, j+1, length+1))
                     cache[(i, j)] = res
 
-        return max(res, length)
+        return res
 
     return dfs(0, 0, 0)
