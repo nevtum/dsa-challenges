40243445a743a53b812ae939368dc1e94c56c39e
Neville Tummon <nevilletummon@Nevilles-MacBook-Pro.local>
2025-06-10 23:16:19 +1000

    add a failing test

--- dynamic_programming/buy_and_sell_stock_with_fee/test_algo.py
+++ dynamic_programming/buy_and_sell_stock_with_fee/test_algo.py
@@ -4,6 +4,7 @@ from .algo import max_profit
 @pytest.mark.parametrize("prices,fee,expected", [
     ([1,3,2,8,4,9], 2, 8),
     ([1,3,7,5,10,3], 3, 6),
+    ([4,5,2,4,3,3,1,2,5,4], 1, 4),
 ])
 def test_max_profit_buy_and_sell_stock(prices, fee, expected):
     assert max_profit(prices, fee) == expected
