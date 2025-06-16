f5702f6eafa1e118d6a0705a9c1fad512dff9a6e
Neville Tummon <nt.devs@gmail.com>
2025-06-16 19:05:55 +1000

    squeeze a bit more performance, solution to lc 120

--- dynamic_programming/minimum_total/algo.py
+++ dynamic_programming/minimum_total/algo.py
@@ -1,17 +1,20 @@
 from functools import cache
 from typing import List
-from .debug import Tracker
+# from .debug import Tracker
 
 def minimum_total(triangle: List[List[int]]) -> int:
-    debug = Tracker()
+    # debug = Tracker()
     height = len(triangle)
 
     @cache
-    @debug.track
+    # @debug.track
     def dp(depth: int, i: int) -> int:
-        if depth >= height:
-            # if out of bounds, don't add anything
-            return 0
+        if not (0 <= depth < height) or not (0 <= i < len(triangle[depth])):
+            return float("inf")
+
+        if depth >= height-1:
+            # if bottom of the triangle, return current
+            return triangle[depth][i]
 
         # current node value + best subproblem solution
         res = triangle[depth][i] + min(dp(depth+1, i), dp(depth+1, i+1))
