f33c2c9961af0a13bf3ce5b3c628fa96f4840e80
Neville Tummon <nt.devs@gmail.com>
2025-06-16 20:14:31 +1000

    unoptimised dp solution to lc 64

--- dynamic_programming/minimum_path_sum/algo.py
+++ dynamic_programming/minimum_path_sum/algo.py
@@ -1,4 +1,29 @@
 from typing import List
 
 def min_path_sum(grid: List[List[int]]) -> int:
-    raise NotImplementedError()
+    n, m = len(grid), len(grid[0])
+    print(f"{n}x{m}")
+
+    def valid(i: int, j: int) -> bool:
+        return 0 <= i < n and 0 <= j < m
+
+    def dp(i: int, j: int) -> int:
+        if not valid(i, j):
+            return 0
+
+        best = float('inf')
+        valid_dir = False
+
+        # right or down
+        for di, dj in ((0, 1),(1, 0)):
+            ii, jj = i+di, j+dj
+            if valid(ii, jj):
+                valid_dir = True
+                best = min(best, dp(ii, jj))
+
+        if not valid_dir:
+            return grid[i][j]
+        else:
+            return grid[i][j] + int(best)
+
+    return dp(0, 0)
--- dynamic_programming/minimum_path_sum/test_algo.py
+++ dynamic_programming/minimum_path_sum/test_algo.py
@@ -3,7 +3,8 @@ from .algo import min_path_sum
 
 @pytest.mark.parametrize("grid, expected", [
     ([[12]], 12),
-    ([[1,2,3], 6]),
+    ([[1,2,3]], 6),
+    ([[1,3,1],[1,5,1],[4,2,1]], 7),
 ])
 def test_min_path_sum(grid, expected):
     assert min_path_sum(grid) == expected
