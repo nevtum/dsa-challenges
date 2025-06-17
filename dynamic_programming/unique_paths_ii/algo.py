9b1b844a89f112a6f9e075f88d82231cc5b3b548
Neville Tummon <nt.devs@gmail.com>
2025-06-17 22:53:22 +1000

    dp solution for lc 63

--- dynamic_programming/unique_paths_ii/algo.py
+++ dynamic_programming/unique_paths_ii/algo.py
@@ -1,4 +1,29 @@
 from typing import List
 
 def unique_paths_with_obstacles(grid: List[List[int]]) -> int:
-    raise NotImplementedError("unique_paths_with_obstacles is not implemented")
+    n, m = len(grid), len(grid[0])
+    paths = [[0] * m for _ in range(n)]
+    paths[0][0] = 1
+
+    # if entry is the same as exit
+    if n == m == 1:
+        # then check that there is no obstacle
+        return 1 if grid[0][0] == 0 else 0
+
+    for i_ref in range(n):
+        for j_ref in range(m):
+            # if current position is an obstacle, then skip
+            if grid[i_ref][j_ref] == 1:
+                continue
+
+            # can only travel from up or left position
+            for di, dj in ((0, -1), (-1, 0)):
+                ii, jj = i_ref + di, j_ref + dj
+                # if outside of bounds or travelling from an obstacle
+                if ii < 0 or jj < 0 or grid[ii][jj] == 1:
+                    continue
+
+                # add different ways to reach paths[ii][jj]
+                paths[i_ref][j_ref] += paths[ii][jj]
+
+    return paths[-1][-1]
--- dynamic_programming/unique_paths_ii/test_algo.py
+++ dynamic_programming/unique_paths_ii/test_algo.py
@@ -2,9 +2,19 @@ import pytest
 from .algo import unique_paths_with_obstacles
 
 @pytest.mark.parametrize("grid, expected", [
-    ([[0, 0, 0], [0, 1, 0], [0, 0, 0]], 2),
-    ([[0, 1], [0, 0]], 1),
-    ([[1, 0], [0, 0]], 0)
+    ([[0,0,0,0]], 1),
+    ([[0,0],[0,0]], 2),
+    ([[0,1],[0,0]], 1),
+    ([[0,0,0],[0,1,0],[0,0,0]], 2),
+    ([[0,0],[0,1]], 0),
+    ([[0,1],[1,0]], 0),
+    ([[1]], 0),
+    ([[0]], 1),
+    ([[1,0],[0,0]], 0),
+    ([[1,1],[1,1]], 0),
 ])
 def test_unique_paths_with_obstacles(grid, expected):
     assert unique_paths_with_obstacles(grid) == expected
+
+if __name__ == "__main__":
+    pytest.main([__file__, '-sv'])
