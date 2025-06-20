9aaa73659d8bb9cc0aabdb6a9bfa2aef07645ac8
Neville Tummon <nt.devs@gmail.com>
2025-06-20 19:59:52 +1000

    backtracking solution for lc 211

--- trie/add_and_search_words/algo.py
+++ trie/add_and_search_words/algo.py
@@ -1,3 +1,7 @@
+from utils.debug import CallTracker
+
+tracker = CallTracker()
+
 class Node:
     def __init__(self):
         self.children = {}
@@ -9,32 +13,36 @@ class Node:
 class WordDictionary:
     def __init__(self):
         self.root = Node()
-        self.root.children['.'] = Node()
 
     def addWord(self, word: str) -> None:
         node = self.root
-        wildcard_node = self.root.children['.']
         for ch in word:
-            if not '.' in wildcard_node.children:
-                wildcard_node.children['.'] = Node()
-
-            if not ch in node.children:
+            if ch not in node.children:
                 node.children[ch] = Node()
 
-            wildcard_node.children[ch] = node.children[ch]
-            node.children['.'] = wildcard_node.children['.']
-
-            wildcard_node = wildcard_node.children['.']
             node = node.children[ch]
 
         node.end_word = True
 
     def search(self, word: str) -> bool:
-        node = self.root
-        for ch in word:
-            print(f"looking for {ch} in {node}")
-            if ch in node.children:
-                node = node.children[ch]
-            else:
-                return False
+        return dfs_search(word, 0, self.root)
+
+@tracker.track
+def dfs_search(word: str, index: int, node: Node) -> bool:
+    assert index <= len(word)
+
+    if index == len(word):
         return True
+
+    ch = word[index]
+
+    print(f"looking for {ch} in {node}")
+
+    # in the case of wildcard
+    if ch == '.':
+        # find at least one match where a sub search on child matches the character after '.'
+        return any(dfs_search(word, index+1, child_node) for child_node in node.children.values())
+    else:
+        if ch not in node.children:
+            return False
+        return dfs_search(word, index+1, node.children[ch])
--- trie/add_and_search_words/test_algo.py
+++ trie/add_and_search_words/test_algo.py
@@ -6,16 +6,16 @@ from .algo import WordDictionary
 @pytest.fixture()
 def word_dictionary() -> WordDictionary:
     wd = WordDictionary()
-    wd.addWord("bad");
-    wd.addWord("dad");
-    wd.addWord("mad");
+    wd.addWord("bad")
+    wd.addWord("dad")
+    wd.addWord("mad")
     wd.addWord("mid")
     return wd
 
 @pytest.mark.parametrize("keyword, expected", [
     ("pad", False),
     ("bad", True),
-    # (".ad", True),
+    (".id", True),
     ("..d", True),
     ("..c", False),
     ("b..", True),
@@ -23,11 +23,6 @@ def word_dictionary() -> WordDictionary:
 ])
 def test_search(word_dictionary, keyword, expected):
     assert word_dictionary.search(keyword) == expected
-
-def test_composition(word_dictionary):
-    assert set(word_dictionary.root.children.keys()) == {'.', 'b', 'd', 'm'}
-    wc_node = word_dictionary.root.children['.']
-    assert set(wc_node.children.keys()) == {'.', 'a', 'i'}
 
 if __name__ == "__main__":
     pytest.main([__file__, "-sv"])
