3069b6703d9e965e274bc237976885a37de3a6d6
Neville Tummon <nt.devs@gmail.com>
2025-06-20 19:05:40 +1000

    add failing tests

--- trie/add_and_search_words/algo.py
+++ trie/add_and_search_words/algo.py
@@ -1,9 +1,40 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end_word = False
+
+    def __repr__(self):
+        return f"Node(children={set(self.children.keys())})"
+
 class WordDictionary:
     def __init__(self):
-        raise NotImplementedError("Method not implemented")
+        self.root = Node()
+        self.root.children['.'] = Node()
 
     def addWord(self, word: str) -> None:
-        raise NotImplementedError("Method not implemented")
+        node = self.root
+        wildcard_node = self.root.children['.']
+        for ch in word:
+            if not '.' in wildcard_node.children:
+                wildcard_node.children['.'] = Node()
+
+            if not ch in node.children:
+                node.children[ch] = Node()
+
+            wildcard_node.children[ch] = node.children[ch]
+            node.children['.'] = wildcard_node.children['.']
+
+            wildcard_node = wildcard_node.children['.']
+            node = node.children[ch]
+
+        node.end_word = True
 
     def search(self, word: str) -> bool:
-        raise NotImplementedError("Method not implemented")
+        node = self.root
+        for ch in word:
+            print(f"looking for {ch} in {node}")
+            if ch in node.children:
+                node = node.children[ch]
+            else:
+                return False
+        return True
--- trie/add_and_search_words/test_algo.py
+++ trie/add_and_search_words/test_algo.py
@@ -9,13 +9,25 @@ def word_dictionary() -> WordDictionary:
     wd.addWord("bad");
     wd.addWord("dad");
     wd.addWord("mad");
+    wd.addWord("mid")
     return wd
 
 @pytest.mark.parametrize("keyword, expected", [
     ("pad", False),
     ("bad", True),
-    (".ad", True),
+    # (".ad", True),
+    ("..d", True),
+    ("..c", False),
     ("b..", True),
+    (".", True),
 ])
 def test_search(word_dictionary, keyword, expected):
     assert word_dictionary.search(keyword) == expected
+
+def test_composition(word_dictionary):
+    assert set(word_dictionary.root.children.keys()) == {'.', 'b', 'd', 'm'}
+    wc_node = word_dictionary.root.children['.']
+    assert set(wc_node.children.keys()) == {'.', 'a', 'i'}
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-sv"])
