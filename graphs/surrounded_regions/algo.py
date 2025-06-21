129b28bc93e7bc946bec94f82e2de645af6feef9
Neville Tummon <nt.devs@gmail.com>
2025-06-21 18:39:22 +1000

    dfs + bfs solution to lc 130

--- graphs/surrounded_regions/algo.py
+++ graphs/surrounded_regions/algo.py
@@ -1,8 +1,6 @@
 from typing import List
-from utils.debug import CallTracker
 from collections import deque
 
-tracker = CallTracker()
 
 def solve(board: List[List[str]]):
     n, m = len(board), len(board[0])
@@ -15,26 +13,31 @@ def solve(board: List[List[str]]):
     def valid(i: int, j: int) -> bool:
         return (0 <= i < n) and (0 <= j < m)
 
-    def dfs_mark_visited(i: int, j: int):
+    def dfs_mark_invalid(i: int, j: int):
         if not valid(i, j) or (i, j) in visited:
             return
 
         visited[(i, j)] = True
 
+        if board[i][j] == "X":
+            return
+
         for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             # visit nearby positions adjacent to edge position
-            dfs_mark_visited(i + di, j + dj)
+            dfs_mark_invalid(i + di, j + dj)
 
     for i in range(n):
         for j in range(m):
             if board[i][j] == "O" and edge(i, j):
-                dfs_mark_visited(i, j)
+                dfs_mark_invalid(i, j)
             elif board[i][j] == "X":
                 visited[(i, j)] = True
             else:
+                # candidates to be flipped
                 q.append((i, j))
 
     while q:
         i, j = q.popleft()
         if (i, j) not in visited:
+            # flip those not visited
             board[i][j] = "X"
