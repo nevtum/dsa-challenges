f0674063944ac5d10a58484149a4dd32927f3ee0
Neville Tummon <nt.devs@gmail.com>
2025-06-22 22:30:58 +1000

    fix bugs in solution for lc 1584

--- graphs/min_cost_connect_all_points/algo.py
+++ graphs/min_cost_connect_all_points/algo.py
@@ -9,24 +9,28 @@ def min_cost_connect_points(points: List[List[int]]) -> int:
     min_heap = [(0, 0)] # start with first point
 
     # O(n)
-    while min_heap:
-        # always picks the minimum distance
+    while len(visited) < n:
+        # Always pick the minimum distance edge
         dist, i = heapq.heappop(min_heap)
 
         if i in visited:
             continue
 
-        min_cost += dist
         visited.add(i)
         print(f"Visited point {i}")
+        min_cost += dist
 
         xi, yi = points[i]
+        # Explore connections to all unvisited points
         # O(n) * O(n) ~ O(n^2)
-        for j in range(i+1, n):
+        for j in range(n):
+            if j in visited:
+                continue
+
             xj, yj = points[j]
             distance = abs(xi - xj) + abs(yi - yj)
             print(f"Distance from point {i} to point {j}: {distance}")
-            # O(n^2) * O(log n) ~ O(n^2 log n)
+            # O(log n) * O(n^2) ~ O(n^2 log n)
             heapq.heappush(min_heap, (distance, j))
 
     return min_cost
