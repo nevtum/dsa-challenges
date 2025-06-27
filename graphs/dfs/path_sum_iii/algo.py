44e2e5744923a2f1c5a77f928e8163e8b550bcc3
Neville Tummon <nt.devs@gmail.com>
2025-06-27 19:48:17 +1000

    add skeleton for optimised algo

--- graphs/dfs/path_sum_iii/algo.py
+++ graphs/dfs/path_sum_iii/algo.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import defaultdict
 
 
 class TreeNode:
@@ -12,6 +13,44 @@ class TreeNode:
 
 
 def path_sum(root: Optional[TreeNode], target_sum: int) -> int:
+    def count_paths(node: Optional[TreeNode], current_sum: int) -> int:
+        if not node:
+            return 0
+
+        sum_path = current_sum + node.val
+
+        count = (sum_path == target_sum) & 1
+
+        count += count_paths(node.left, sum_path)
+        count += count_paths(node.right, sum_path)
+
+        return count
+
+    def dfs(node: Optional[TreeNode], current_sum: int) -> int:
+        if not node:
+            return 0
+
+        sum_path = current_sum + node.val
+
+        count = (sum_path == target_sum) & 1
+
+        # explore continuing paths through left and right children
+        count += dfs(node.left, sum_path)
+        count += dfs(node.right, sum_path)
+
+        # additionally, branch out new sub solutions starting from children
+        count += count_paths(node.left, 0)
+        count += count_paths(node.right, 0)
+
+        return count
+
+    return dfs(root, 0)
+
+
+def path_sum_optimised(root: Optional[TreeNode], target_sum: int) -> int:
+    lookup = defaultdict(int)
+    lookup[target_sum] = 1
+
     def count_paths(node: Optional[TreeNode], current_sum: int) -> int:
         if not node:
             return 0
--- graphs/dfs/path_sum_iii/test_algo.py
+++ graphs/dfs/path_sum_iii/test_algo.py
@@ -1,17 +1,18 @@
 import pytest
 from typing import Optional, List
-from .algo import TreeNode, path_sum
+from .algo import TreeNode, path_sum, path_sum_optimised
 
-@pytest.mark.parametrize("root, target_sum, expected", [
+@pytest.mark.parametrize("array, target_sum, expected", [
     ([5,2,None,1], 8, 1),
     ([8,-2,None,2], 8, 2),
     ([10,5,-3,3,2,None,11,3,-2,None,1], 8, 3),
     ([5,4,8,11,None,13,4,7,2,None,None,5,1], 22, 3),
     ([1,None,2,None,3,None,4,None,5], 3, 2),
 ])
-def test_path_sum(root, target_sum, expected):
-    tree = build_tree(root)
-    assert path_sum(tree, target_sum) == expected
+def test_path_sum(array, target_sum, expected):
+    root = build_tree(array)
+    assert path_sum(root, target_sum) == expected
+    assert path_sum_optimised(root, target_sum) == expected
 
 def build_tree(root: List[Optional[int]]) -> Optional[TreeNode]:
     if not root:
