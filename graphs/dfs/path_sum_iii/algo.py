9aaef21c0e3c7014c1b7e8bf4a2ab8e54850ba89
Neville Tummon <nt.devs@gmail.com>
2025-06-27 19:35:11 +1000

    fix algo for lc 437

--- graphs/dfs/path_sum_iii/algo.py
+++ graphs/dfs/path_sum_iii/algo.py
@@ -1,7 +1,5 @@
 from typing import Optional
-from utils.debug import CallTracker
 
-tracker = CallTracker()
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -14,22 +12,34 @@ class TreeNode:
 
 
 def path_sum(root: Optional[TreeNode], target_sum: int) -> int:
-    # @tracker.track
-    def dfs(node: Optional[TreeNode], total: int) -> int:
+    def count_paths(node: Optional[TreeNode], current_sum: int) -> int:
         if not node:
             return 0
 
-        count = 0
-        sum_path = total + node.val
-        # dfs path sum equals the target sum
-        if sum_path == target_sum:
-            count += 1
+        sum_path = current_sum + node.val
 
-        # and count all paths that continue their path sum to equal target_sum
-        count += dfs(node.left, sum_path) + dfs(node.right, sum_path)
+        count = (sum_path == target_sum) & 1
 
-        # and count all paths where nodes left or right starts with total = 0
-        count += dfs(node.left, 0) + dfs(node.right, 0)
+        count += count_paths(node.left, sum_path)
+        count += count_paths(node.right, sum_path)
+
+        return count
+
+    def dfs(node: Optional[TreeNode], current_sum: int) -> int:
+        if not node:
+            return 0
+
+        sum_path = current_sum + node.val
+
+        count = (sum_path == target_sum) & 1
+
+        # explore continuing paths through left and right children
+        count += dfs(node.left, sum_path)
+        count += dfs(node.right, sum_path)
+
+        # additionally, branch out new sub solutions starting from children
+        count += count_paths(node.left, 0)
+        count += count_paths(node.right, 0)
 
         return count
 
