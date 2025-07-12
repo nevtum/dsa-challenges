837a6733d7fdb3a99d7e79c0f3568ea0cbfea28c
Neville Tummon <nt.devs@gmail.com>
2025-07-12 23:28:04 +1000

    fix wrong test case and tighten up algo for lc 88

--- two_pointers/merge_sorted_array/algo.py
+++ two_pointers/merge_sorted_array/algo.py
@@ -6,16 +6,11 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 
     mm, nn = m - 1, n - 1
     last = m + n - 1
-    while mm >= 0 and nn >= 0:
+    while nn >= 0:
         if nums1[mm] >= nums2[nn]:
             nums1[last] = nums1[mm]
             mm -= 1
         else:
             nums1[last] = nums2[nn]
             nn -= 1
-        last -= 1
-
-    while nn >= 0:
-        nums1[last] = nums2[nn]
-        nn -= 1
         last -= 1
--- two_pointers/merge_sorted_array/test_algo.py
+++ two_pointers/merge_sorted_array/test_algo.py
@@ -9,7 +9,7 @@ from .algo import merge
         ([0], 0, [1], 1, [1]),
         ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]),
         ([1, 2, 5, 0, 0, 0], 3, [2, 3, 6], 3, [1, 2, 2, 3, 5, 6]),
-        ([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3, [-1, 1, 2, 2, 3, 3, 3]),
+        ([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3, [-1, 0, 0, 1, 2, 2, 3, 3, 3]),
     ],
 )
 def test_merge_sorted_array(nums1, n, nums2, m, expected):
@@ -18,4 +18,4 @@ def test_merge_sorted_array(nums1, n, nums2, m, expected):
 
 
 if __name__ == "__main__":
-    pytest.main([__file__, "-sv"])
+    pytest.main([__file__, "-svv"])
