8c283c97b0fd939e6597c3ad3b670c6faed21480
Neville Tummon <nt.devs@gmail.com>
2025-07-13 13:03:24 +1000

    better solution for lc 88

--- two_pointers/merge_sorted_array/algo.py
+++ two_pointers/merge_sorted_array/algo.py
@@ -6,11 +6,17 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 
     mm, nn = m - 1, n - 1
     last = m + n - 1
-    while nn >= 0:
+    while mm >= 0 and nn >= 0:
         if nums1[mm] >= nums2[nn]:
             nums1[last] = nums1[mm]
             mm -= 1
         else:
             nums1[last] = nums2[nn]
             nn -= 1
+        last -= 1
+
+    # if remaining numbers left to merge from nums2
+    while nn >= 0:
+        nums1[last] = nums2[nn]
+        nn -= 1
         last -= 1
