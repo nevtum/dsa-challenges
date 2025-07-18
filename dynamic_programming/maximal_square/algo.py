ffb75079f5e59d3cf03d1e90b5cf6d1581a1b319
Neville Tummon <nt.devs@gmail.com>
2025-07-18 14:11:19 +1000

    clean up formatting

--- dynamic_programming/maximal_square/algo.py
+++ dynamic_programming/maximal_square/algo.py
@@ -12,15 +12,19 @@ def maximal_square(matrix: List[List[str]]) -> int:
         if dp[i][j] > 0:
             return dp[i][j]
 
-        edges = (
+        # we know at this point that size is at least 1
+        dp[i][j] = 1
+
+        dirs = (
             (i + 1, j), # down
             (i, j + 1), # right
             (i + 1, j + 1), # diagonal
         )
-        neighs = [dfs(ii, jj) for ii, jj in edges]
-        if 0 in neighs:
-            dp[i][j] = 1
-        else:
+        neighs = [dfs(ii, jj) for ii, jj in dirs]
+
+        # if neighbour from every direction has non zero size
+        # then we can expand the size by 1 at the current position
+        if not 0 in neighs:
             dp[i][j] = 1 + min(neighs)
 
         return dp[i][j]
