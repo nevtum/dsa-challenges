cead0f0e281244a2eb62e28e2a310d52a3c85f59
Neville Tummon <nt.devs@gmail.com>
2025-07-19 22:08:06 +1000

    boilerplate test for lc 338

---
+++ counting_bits/algo.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def count_bits(n: int) -> List[int]:
+    raise NotImplementedError("Implement this function")
---
+++ counting_bits/test_algo.py
@@ -0,0 +1,17 @@
+import pytest
+from .algo import count_bits
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (2, [0, 1, 1]),
+        (5, [0, 1, 1, 2, 1, 2]),
+    ],
+)
+def test_count_bits(n, expected):
+    assert count_bits(n) == expected
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-sv"])
