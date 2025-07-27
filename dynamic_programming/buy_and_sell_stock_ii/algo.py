b1eee0cd58898eca470b7246e0c32488afb077d9
Neville Tummon <nt.devs@gmail.com>
2025-07-27 20:43:07 +1000

    solution for lc 122

--- dynamic_programming/buy_and_sell_stock_ii/algo.py
+++ dynamic_programming/buy_and_sell_stock_ii/algo.py
@@ -1,5 +1,15 @@
 from typing import List
 
 
+# this solution is not necessarily a dp algo, but a greedy one
+# since the optimal solution will always be selling higher than
+# the entry position.
 def max_profit(prices: List[int]) -> int:
-    raise NotImplementedError()
+    profit = 0
+
+    for i in range(1, len(prices)):
+        # while price movement is going up
+        if prices[i] > prices[i-1]:
+            profit += prices[i] - prices[i-1]
+
+    return profit
--- dynamic_programming/buy_and_sell_stock_ii/test_algo.py
+++ dynamic_programming/buy_and_sell_stock_ii/test_algo.py
@@ -5,7 +5,7 @@ from .algo import max_profit
 @pytest.mark.parametrize(
     "prices, expected",
     [
-        ([7, 1, 5, 3, 6, 4], 5),
+        ([7, 1, 5, 3, 6, 4], 7),
         ([1, 2, 3, 4, 5], 4),
         ([7, 6, 4, 3, 1], 0),
     ],
