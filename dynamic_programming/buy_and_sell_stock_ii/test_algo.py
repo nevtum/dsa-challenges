847d309595bf1baf1740feff8c5595c80c87aef0
Neville Tummon <nt.devs@gmail.com>
2025-07-27 19:38:48 +1000

    boilerplate for lc 122

---
+++ dynamic_programming/buy_and_sell_stock_ii/test_algo.py
@@ -0,0 +1,18 @@
+import pytest
+from .algo import max_profit
+
+
+@pytest.mark.parametrize(
+    "prices, expected",
+    [
+        ([7, 1, 5, 3, 6, 4], 5),
+        ([1, 2, 3, 4, 5], 4),
+        ([7, 6, 4, 3, 1], 0),
+    ],
+)
+def test_max_profit(prices, expected):
+    assert max_profit(prices) == expected
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-sv"])
